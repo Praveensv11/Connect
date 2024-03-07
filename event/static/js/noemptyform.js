c5e38c0914107e2d501dc1dd196ceaa76f90c51f
Praveen <praveensv799@gmail.com>
2024-03-07 22:03:38 +0530

    user cannot submit empty form during reporting

---
+++ event/static/js/noemptyform.js
@@ -0,0 +1,11 @@
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelector('#submit').disabled = true;
+
+        document.querySelector('#noempty').onkeyup = () => {
+            if (document.querySelector('#noempty').value.length > 0) {
+                document.querySelector('#submit').disabled = false;
+            } else {
+                document.querySelector('#submit').disabled = true;
+            }
+        };
+    });
